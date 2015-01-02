14a035f297cd437aba1fee4938198f866ee1bae2
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2015-01-02 14:53:54 +1300

    configure puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,13 +26,16 @@ set :repo_url, 'git@github.com:nigelramsay/enalytics.git'
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :puma_threads, [1, 10]
+set :puma_init_active_record, true
 
 namespace :deploy do
 
