3f2dba0b18c70e714147395d6f07780ad62b1ede
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2015-01-04 14:21:19 +1300

    refactor karori weather routine

--- app/services/save_karori_weather.rb
+++ app/services/save_karori_weather.rb
@@ -4,39 +4,55 @@ class SaveKaroriWeather
   end
 
   def save
-    save_current_conditions
+    save_temperature_conditions if updated_temperature_data?
+    save_wind_conditions if updated_wind_data?
+  end
+
+  def updated_wind_data?
+    latest_wind_measurement.nil? || station_data_updated_at > latest_wind_measurement.recorded_at
+  end
+
+  def updated_temperature_data?
+    latest_temperature_measurement.nil? || station_data_updated_at > latest_temperature_measurement.recorded_at
   end
 
   private
 
   attr_reader :station_id
 
-  def current_temperature
-    harvest.temperature
-  end
-
-  def recorded_at
+  def station_data_updated_at
     harvest.recorded_at
   end
 
-  def latest_measurement
+  def latest_temperature_measurement
     Measurement.where(code: 'karori_temp').order('recorded_at desc').first
   end
 
-  def save_current_conditions
-    if latest_measurement.nil? || recorded_at > latest_measurement.recorded_at
-      measurement = Measurement.create! value: current_temperature, code: 'karori_temp', recorded_at: recorded_at
-      $stderr.puts "Karori data... current_temperature #{measurement.value} recorded_at #{measurement.recorded_at}"
+  def latest_wind_measurement
+    WindMeasurement.where(code: 'karori').order('recorded_at desc').first
+  end
 
-      wind = WindMeasurement.create! code: 'karori',
-                                     speed: harvest.wind_speed,
-                                     gust_speed: harvest.wind_gust,
-                                     direction: harvest.wind_direction,
-                                     recorded_at: harvest.recorded_at
-      $stderr.puts "Karori wind... speed #{wind.speed} #{wind.direction} gust #{wind.gust_speed}"
-    else
-      $stderr.puts "No new Karori records"
-    end
+  def save_wind_conditions
+    wind = WindMeasurement.find_or_initialize_by code: 'karori',
+                                                 recorded_at: harvest.recorded_at
+
+    wind.speed = harvest.wind_speed
+    wind.gust_speed = harvest.wind_gust
+    wind.direction = harvest.wind_direction
+
+    wind.save!
+
+    $stderr.puts "Karori wind... speed #{wind.speed} #{wind.direction} gust #{wind.gust_speed}"
+  end
+
+  def save_temperature_conditions
+    measurement = Measurement.find_or_initialize_by code: 'karori_temp', recorded_at: station_data_updated_at
+
+    measurement.value = harvest.temperature
+
+    measurement.save!
+
+    $stderr.puts "Karori data... current_temperature #{measurement.value} recorded_at #{measurement.recorded_at}"
   end
 
   def harvest
