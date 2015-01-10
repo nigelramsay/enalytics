1776fa390c68ffc70f0ef76ae922b9f8fdaccd30
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2015-01-10 21:41:13 +1300

    ignore seconds on the heatmiser values

--- app/services/save_heatmiser_values.rb
+++ app/services/save_heatmiser_values.rb
@@ -1,42 +1,49 @@
 class SaveHeatmiserValues
 	def initialize(args={})
 	end
-	
+
   def save
     save_current_temperature
     save_set_temperature
     save_heating_status
-  end	
+  end
 
   private
 
   def save_current_temperature
     if temp = heatmiser.current_temperature
-      Measurement.create! value: temp, code: 'actual', recorded_at: Time.current
+      Measurement.create! value: temp, code: 'actual', recorded_at: current_time_without_seconds
     else
       $stderr.puts "Unable to acquire current_temperature"
-    end    
+    end
   end
 
   def save_set_temperature
     if temp = heatmiser.set_temperature
-      Measurement.create! value: temp, code: 'set', recorded_at: Time.current
+      Measurement.create! value: temp, code: 'set', recorded_at: current_time_without_seconds
     else
       $stderr.puts "Unable to acquire set_temperature"
-    end    
+    end
   end
 
   def save_heating_status
     heating_status = heatmiser.heating?
 
     if heating_status != nil
-      Measurement.create! value: heating_status ? '1' : '0', code: 'on', recorded_at: Time.current
+      Measurement.create! value: heating_status ? '1' : '0', code: 'on', recorded_at: current_time_without_seconds
     else
       $stderr.puts "Unable to acquire heating_status"
-    end    
+    end
   end
 
   def heatmiser
     @heatmiser ||= HeatMiser.new
+  end
+
+  def current_time_without_seconds
+    @_time ||= begin
+      now = Time.current
+      Time.new(now.year, now.month, now.day, now.hour, now.min)
+    end
   end
 end
