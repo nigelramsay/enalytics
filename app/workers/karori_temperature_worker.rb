71bdf2cdfdcbbd69a02adda9aa8c93d6286f5c6d
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2015-01-16 20:01:32 +1300

    restrict worker retries

--- app/workers/karori_temperature_worker.rb
+++ app/workers/karori_temperature_worker.rb
@@ -2,6 +2,8 @@ class KaroriTemperatureWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
+  sidekiq_options retry: 3
+
   recurrence do
     hourly.minute_of_hour(0, 15, 30, 45)
   end
--- app/workers/measurement_worker.rb
+++ app/workers/measurement_worker.rb
@@ -2,6 +2,8 @@ class MeasurementWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
+  sidekiq_options retry: 2
+
   recurrence do
     minutely(1)
   end
